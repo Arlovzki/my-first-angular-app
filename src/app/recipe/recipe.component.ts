ca963332ef67bccce25146b327f0d4df22c42e0d
Arlovzki <arloudev@gmail.com>
2021-01-19 19:08:45 +0800

    1st

---
+++ src/app/recipe/recipe.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Output } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Component({
+  selector: 'app-recipe',
+  templateUrl: './recipe.component.html',
+  styleUrls: ['./recipe.component.css']
+})
+export class RecipeComponent implements OnInit {
+ @Output() selectedRecipe! : Recipe;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  
+
+}
